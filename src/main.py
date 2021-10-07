c4ba099a20ac03d4f2463ac852e0d37dfce97da0
Oskar Hallström <oskar@MacBook-Air-som-tillhor-Oskar.local>
2021-10-08 01:14:53 +0200

    Updating structure

---
+++ src/main.py
@@ -0,0 +1,86 @@
+'''
+File name: main.py
+Author: Oskar Hallström
+Date created: 19/07/2021
+Date last modified: 08/10/2021
+Python Version: 3.8
+'''
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import numpy as np
+from sklearn import cluster as skc
+import spotify_helpers as sh
+
+# Setting constants
+MIN_THRESHOLD = 10
+SHOULD_DELETE_PREVIOUS = True
+ATTRIBUTE_LABELS = [
+    ('valence',1),
+    ('energy',1),
+    ('acousticness',1),
+    ('danceability',1),
+    ('instrumentalness',1),
+    ('speechiness', 1),
+    ('tempo', 1/150),
+    ('mode', 1)
+    ]
+PLAYLIST_DESCRIPTION = 'Automatically generated playlist by clustering of my liked songs.'
+
+
+if __name__ == '__main__':
+    # initialize client and get id of current user
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyOAuth(
+            client_id="b049aec063a34f9a82736498da19de3f",
+            client_secret="a181cae4c2dd4a7ab5c88eef0239edb7",
+            redirect_uri="http://localhost/",
+            scope="user-library-read playlist-modify-private playlist-read-private playlist-modify-public user-follow-modify"))
+    user = sp.me()['id']
+    
+    # get all the attributes, ids and names of all saved songs
+    (attributes, ids, names) = sh.get_features_of_saved_songs(sp, ATTRIBUTE_LABELS)
+
+    # create the model and the clusters
+    model = skc.AgglomerativeClustering(
+        n_clusters=None,
+        linkage='complete',
+        distance_threshold=0.35
+        )
+    model.fit(attributes)
+
+    #add track_ids to each cluster
+    clusters = [[] for i in range(model.n_clusters_)]
+    clusters_attributes = [[] for i in range(model.n_clusters_)]
+    clusters_names = [[] for i in range(model.n_clusters_)]
+    for idx, label in enumerate(model.labels_):
+        clusters[label].append(ids[idx])
+        clusters_attributes[label].append(attributes[idx])
+        clusters_names[label].append(names[idx])
+
+    #filter away clusters smaller than MIN_THRESHOLD and get their names
+    playlists_to_create = []
+    for i in range(model.n_clusters_):
+        # filter away too small clusters
+        if len(clusters[i]) > MIN_THRESHOLD:
+            name = sh.get_playlist_name(np.array(clusters_attributes[i]), clusters_names[i])
+            playlists_to_create.append((name, clusters[i]))
+
+    if SHOULD_DELETE_PREVIOUS:
+        sh.delete_previously_generated_lists(sp, PLAYLIST_DESCRIPTION)
+
+    #create playlists
+    for name, _ in playlists_to_create:
+        sh.create_playlist(sp, user, name, PLAYLIST_DESCRIPTION)
+
+    #get ids of created playlists
+    playlists = sp.current_user_playlists()
+    playlist_ids = {}
+    for i, playlist in enumerate(playlists['items']):
+        if playlist['description'] == PLAYLIST_DESCRIPTION:
+            playlist_ids[playlist['name']] = playlist['id']
+
+    #add tracks to the created playlists
+    print("Creating the following playlists: ")
+    for playlist in playlists_to_create:
+        print(playlist[0])
+        sh.add_tracks(sp, user, playlist[0], playlist[1], playlist_ids[playlist[0]], 0)
---
+++ src/spotify_helpers.py
@@ -0,0 +1,128 @@
+'''
+File name: spotify_helpers.py
+Author: Oskar Hallström
+Date created: 08/10/2021
+Date last modified: 08/10/2021
+Python Version: 3.8
+'''
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import numpy as np
+from sklearn import cluster as skc
+import spotify_helpers
+
+def get_features_of_saved_songs(client, attribute_labels):
+    """
+    Gets features of all saved songs in the user's library
+    :param client: Spotify API client
+    :param attribute_labels: list of song attributes to get
+    :return: 
+        - attributes - list of attributes of saved songs
+        - ids - list of ids of saved songs
+        - names - list of names of saved songs
+    """
+    offset = 0
+    list_attributes = []
+    list_ids = []
+    list_names = []
+
+    while True:
+        songs = client.current_user_saved_tracks(limit=50, offset=offset)['items']
+
+        if len(songs) == 0:
+            break
+
+        ids = [i['track']['id'] for i in songs]
+        names = [i['track']['name'] for i in songs]
+        features = client.audio_features(ids)
+        attributes = np.ndarray(shape=(len(ids), len(attribute_labels)))
+        offset += 50
+
+        for j, feature in enumerate(features):
+            for i, label in enumerate(attribute_labels):
+                if feature == None:
+                    print("Was not possible to get attributes from " + names[i])
+                    attributes[j,i] = 0
+                else:
+                    attributes[j,i] = feature[label[0]] * label[1]
+
+        list_attributes.append(attributes)
+        list_ids.append(ids)
+        list_names.append(names)
+    return (np.ma.concatenate(list_attributes), np.ma.concatenate(list_ids), np.ma.concatenate(list_names))
+
+def delete_previously_generated_lists(client, description):
+    """
+    Deletes playlists with matching description as well
+    as empty playlists with names starting with "Vibes of"
+    :param client: Spotify API client
+    :param description: String used for playlist descriptions
+    """
+    offset = 0
+
+    while True:
+        playlists = client.current_user_playlists(limit=50, offset=offset)['items']
+        if len(playlists) == 0:
+            break
+
+        for playlist in playlists:
+            if playlist['description'] == description:
+                client.current_user_unfollow_playlist(playlist['id'])
+                print("Playlist {} has been erased.".format(playlist['name']))
+            elif playlist['name'][:8] == "Vibes of" and playlist['tracks']['total']==0:
+                client.current_user_unfollow_playlist(playlist['id'])
+                print("Playlist {} has been erased.".format(playlist['name']))
+        offset += 50
+
+def create_playlist(client, user, name, description):
+    '''
+    Creates a private playlist according to the given parameters.
+    :param client: Spotify API client
+    :param user: String of the current user's user id
+    :param name: String of the playlist name
+    :param description: String used for playlist descriptions
+    '''
+    client.user_playlist_create(
+        user=user,
+        name=name,
+        public=False,
+        collaborative=False,
+        description=description)
+
+def get_playlist_name(attributes, names):
+    """
+    Gets the playlist name which is based on the name of the approximated medoid of the cluster.
+    The medoid is approximated as the closest point to the centroid of the cluster.
+    :param attributes: list of all the playlist's song attributes
+    :param names: list of all the playlist's song names
+    """
+    centroid = attributes.mean(axis=0)
+    medoid_idx = ((attributes - centroid) ** 2).sum(axis=1).argmin()
+    return "Vibes of " + names[medoid_idx]
+
+def add_tracks(client, user, name, tracks, id, n_try):
+    '''
+    Adds specified tracks to an existing playlist.
+    :param client: Spotify API client
+    :param user: String of the current user's user id
+    :param name: String of the playlist name
+    :param tracks: list of all the ids of the tracks to add
+    :param id: String of the playlist id
+    :param n_try: int of maximum number of tries
+    TODO: Find a more robust solutions for KeyErrors 
+    '''
+    print("Try number " + str(n_try))
+    if n_try == 5:
+        print("Maximum amount of tries has been used.")
+        client.current_user_unfollow_playlist(id)
+        print("Playlist " + name + " has been deleted.")
+    else:
+        try:
+            client.user_playlist_add_tracks(
+                user=user,
+                playlist_id=id,
+                tracks=tracks,
+                position=None)
+        except KeyError:
+            print("An error occured in the creation of " + playlist[0] + ", let's try again!")
+            add_tracks(client, user, name, tracks, id, n_try+1)
